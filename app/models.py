8468edb7b212ee736279b187c174dffbca7ec3d6
zhuwj <byzhubaiyuan@gmail.com>
2025-08-07 16:38:41 +0800

    Add new users from GitHub to our in-memory database

--- app/models.py
+++ app/models.py
@@ -12,3 +12,22 @@ fake_users_db = {
         "disabled": False,
     }
 }
+
+
+def get_or_create_user_from_github(github_user: dict):
+    github_id = github_user.get("id")
+
+    for user in fake_users_db.values():
+        if user.get("github_id") == github_id:
+            return user
+
+    new_user = {
+        "username": github_user.get("login"),
+        "github_id": github_id,
+        "full_name": github_user.get("name"),
+        "email": github_user.get("email"),
+        "hashed_password": None,  # No password for OAuth users
+        "disabled": False,
+    }
+    fake_users_db[new_user["username"]] = new_user
+    return new_user
