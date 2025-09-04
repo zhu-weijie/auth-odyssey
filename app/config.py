cf43f3787f220521bdeac6a306c21ef9649e734a
zhuwj <byzhubaiyuan@gmail.com>
2025-09-04 11:51:38 +0800

    Update Configuration

--- app/config.py
+++ app/config.py
@@ -9,11 +9,13 @@ class Settings(BaseSettings):
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     JWT_ISSUER: str
     JWT_AUDIENCE: str
+    JWT_PRIVATE_KEY: str
+    JWT_PUBLIC_KEY: str
 
     GITHUB_CLIENT_ID: str
     GITHUB_CLIENT_SECRET: str
 
-    ALGORITHM: str = "HS256"
+    ALGORITHM: str = "RS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
 
     model_config = SettingsConfigDict(env_file=".env")
