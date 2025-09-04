5d33c6431e790bf1d97501992bd26b5e86a4d38c
zhuwj <byzhubaiyuan@gmail.com>
2025-09-04 08:39:24 +0800

    Update Configuration

--- app/config.py
+++ app/config.py
@@ -3,15 +3,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     API_KEY: str
+
     JWT_SECRET_KEY: str
+    JWT_REFRESH_SECRET_KEY: str
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    JWT_ISSUER: str
+    JWT_AUDIENCE: str
+
     GITHUB_CLIENT_ID: str
     GITHUB_CLIENT_SECRET: str
 
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-
-    JWT_REFRESH_SECRET_KEY: str
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
 
     model_config = SettingsConfigDict(env_file=".env")
 
